4246479fbcb378fb48c1fa489cde3f3de9961db4
austin green <trentgr33n@icloud.com>
2022-05-21 18:27:58 -0400

    more js

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,29 @@ $(document).ready(function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        // Save text in local storage
+        
         localStorage.setItem(time, text);
     })
+    function timeTracker() {
+        var timeNow = moment().hour();
+        $(".time-block").each(function() {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+
+            else if (blockTIme === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    ]
